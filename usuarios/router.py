348262710d35785ae629c288c8d389ac027af26a
Epicdeivid <mendozadavidgomez29@gmail.com>
2024-06-21 10:07:38 -0400

    actualizacion carpeta usuarios

--- usuarios/router.py
+++ usuarios/router.py
@@ -1,15 +1,28 @@
-from fastapi import APIRouter, Depends
+from typing import Union
+from fastapi import APIRouter, Depends, Request, Form, HTTPException, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
 from database import SessionLocal, engine
-from typing import Annotated
-from fastapi.security import OAuth2PasswordRequestForm
-import usuarios.models as models 
+import usuarios.models as models
 import usuarios.schemas as schemas
 import usuarios.service as service
+import tipo_usuarios.service as tipo_service
+from fastapi.staticfiles import StaticFiles
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 models.Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="iniciar_sesion")
+
+templates = Jinja2Templates(directory="templates")
+
+router.mount("/static", StaticFiles(directory="static"), name="static")
 
 def get_db():
     db = SessionLocal()
@@ -18,11 +31,60 @@ def get_db():
     finally:
         db.close()
 
+@router.get('/registrar', response_class=HTMLResponse)
+def mostrar_formulario_registro(request: Request, db: Session = Depends(get_db)):
+    tipos_usuarios = tipo_service.listar_tipos_usuarios(db)
+    return templates.TemplateResponse("registro.html", {"request": request, "tipos_usuarios": tipos_usuarios})
 
-@router.post('/registrar', response_model=schemas.Usuario)
-def registrar_usuario(usuario: schemas.UsuarioCrear, db: Session = Depends(get_db)):
-    return service.registrar_usuario(db=db, usuario=usuario)
+@router.post('/registrar', response_class=RedirectResponse)
+def registrar_usuario(
+    cedula: str = Form(...),
+    nombres: str = Form(...),
+    apellidos: str = Form(...),
+    nacimiento: str = Form(...),
+    direccion: str = Form(...),
+    correo: str = Form(...),
+    contraseña: str = Form(...),
+    tipo_id: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    usuario = schemas.UsuarioCrear(
+        cedula=cedula,
+        nombres=nombres,
+        apellidos=apellidos,
+        nacimiento=nacimiento,
+        direccion=direccion,
+        correo=correo,
+        contraseña=contraseña,
+        tipo_id=tipo_id
+    )
+    service.registrar_usuario(db=db, usuario=usuario)
+    return RedirectResponse(url='/usuarios/iniciar_sesion', status_code=status.HTTP_303_SEE_OTHER)
 
-@router.post('/iniciar_sesion', response_model=schemas.Usuario)
-def iniciar_sesion(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]): 
-    return 'xlx'
+@router.get('/iniciar_sesion', response_class=HTMLResponse)
+def mostrar_formulario_inicio_sesion(request: Request):
+    return templates.TemplateResponse("iniciarsesion.html", {"request": request})
+
+@router.post('/iniciar_sesion', response_class=HTMLResponse)
+def iniciar_sesion(cedula: str = Form(...), contraseña: str = Form(...), db: Session = Depends(get_db)):
+    usuario = service.autenticar_usuario(db, cedula, contraseña)
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Credenciales incorrectas',
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    tiempo_expiracion = timedelta(minutes=service.ACCESS_TOKEN_EXPIRE_MINUTES)
+    nombre_completo = f'{usuario.nombres} {usuario.apellidos}'
+    token_acceso = service.crear_token_acceso(
+        data={'cedula': usuario.cedula,
+              'nombre_completo': nombre_completo,
+              'tipo_usuario_id': usuario.tipo_id},
+        expires_delta=tiempo_expiracion
+    )
+    return RedirectResponse(url='/', status_code=status.HTTP_303_SEE_OTHER)
+
+
+@router.delete('/usuario/{cedula}', response_model=schemas.Usuario)
+def borrar_usuario(cedula : str, db: Session = Depends(get_db)): 
+    return service.eliminar_usuario(db=db, cedula=cedula)
--- usuarios/service.py
+++ usuarios/service.py
@@ -1,40 +1,83 @@
-from sqlalchemy.orm import Session
-from schemas import Respuesta
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from typing import Union
 import usuarios.models as models
 import usuarios.schemas as schemas
+from sqlalchemy.orm import Session
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "mysecretkey"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def obtener_hash(contraseña):
+    return pwd_context.hash(contraseña)
+
+def verificar_contraseña(contraseña_simple, contraseña_hasheada):
+    return pwd_context.verify(contraseña_simple, contraseña_hasheada)
 
 def registrar_usuario(db: Session, usuario: schemas.UsuarioCrear):
+    hashed_password = obtener_hash(usuario.contraseña)
     db_usuario = models.Usuario(
-        cedula=usuario.cedula, 
-        nombres=usuario.nombres, 
-        apellidos=usuario.apellidos, 
-        direccion=usuario.direccion, 
-        nacimiento=usuario.nacimiento, 
-        correo=usuario.correo, 
-        contraseña=usuario.contraseña, 
-        tipo_id=usuario.tipo_id)
+        cedula=usuario.cedula,
+        nombres=usuario.nombres,
+        apellidos=usuario.apellidos,
+        direccion=usuario.direccion,
+        nacimiento=usuario.nacimiento,
+        correo=usuario.correo,
+        contraseña=hashed_password,
+        tipo_id=usuario.tipo_id
+    )
     db.add(db_usuario)
     db.commit()
     db.refresh(db_usuario)
     return db_usuario
 
-def listar_usuarios(db: Session): 
+def autenticar_usuario(db: Session, cedula: str, contraseña: str):
+    usuario = obtener_usuario(db, cedula)
+    if not usuario:
+        return False
+    if not verificar_contraseña(contraseña, usuario.contraseña):
+        return False
+    return usuario
+
+def crear_token_acceso(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def listar_usuarios(db: Session):
     return db.query(models.Usuario).all()
 
-def buscar_usuario(db: Session, cedula: str): 
+def buscar_usuario(db: Session, cedula: str):
     returned = db.query(models.Usuario).filter(models.Usuario.cedula == cedula).first()
+    if returned is None:
+        return {"ok": False, "mensaje": "User not found"}
+    usuario = schemas.Usuario(
+        cedula=returned.cedula,
+        nombres=returned.nombres,
+        apellidos=returned.apellidos,
+        direccion=returned.direccion,
+        nacimiento=returned.nacimiento,
+        correo=returned.correo,
+        contraseña=returned.contraseña,
+        tipo_id=returned.tipo_id
+    )
+    return {"ok": True, "mensaje": "User found", "data": usuario}
 
-    if returned == None:
-        return Respuesta[schemas.Categoria](ok=False, mensaje='User not found')
+def obtener_usuario(db: Session, cedula: str):
+    return db.query(models.Usuario).filter(models.Usuario.cedula == cedula).first()
 
-    usuario = schemas.Usuario(cedula=returned.cedula, 
-        nombres=returned.nombres, 
-        apellidos=returned.apellidos, 
-        direccion=returned.direccion, 
-        nacimiento=returned.nacimiento, 
-        correo=returned.correo, 
-        contraseña=returned.contraseña, 
-        tipo_id=returned.tipo_id) 
-    
-    return Respuesta[schemas.Categoria](ok=True, mensaje='User not found', data=usuario)
-
+def eliminar_usuario(db: Session, cedula: str):
+    usuario = obtener_usuario(db, cedula)
+    if usuario:
+        db.delete(usuario)
+        db.commit()
+        return usuario
+    return None
