3d48de54d52ca48a6b0fc74bb9a6cca370fa98f3
Epicdeivid <mendozadavidgomez29@gmail.com>
2024-06-24 11:43:02 -0400

    actualizacion schemas

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from typing import Generic, Optional, TypeVar, Union
+"""from typing import Generic, Optional, TypeVar, Union
 from pydantic import BaseModel
 
 from pydantic.generics import GenericModel
@@ -16,4 +16,26 @@ class Token(BaseModel):
     tipo_token : str
 
 class DataToken(BaseModel): 
-    usuario: Union[str, None] = None
+    usuario: Union[str, None] = None"""
+
+
+from typing import Generic, Optional, TypeVar, Union
+from pydantic import BaseModel
+
+from pydantic.generics import GenericModel
+
+DataT = TypeVar('DataT')
+
+class Respuesta(GenericModel, Generic[DataT]):
+    ok: bool
+    mensaje: str
+    data: Optional[DataT] = None
+
+class Token(BaseModel): 
+    access_token : str
+    token_type : str
+
+class DataToken(BaseModel): 
+    cedula: str
+    nombre_completo: str
+    tipo_usuario_id: int
